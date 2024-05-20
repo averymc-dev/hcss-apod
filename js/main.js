51f5b3a06f5e5e14eaf6124a24450b2acbbfc89c
averymc-dev <averymc.dev@gmail.com>
2024-05-19 20:30:17 -0500

    Added api key env

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
+const apiKey = process.env.API_KEY
+
 let today = new Date();
 let yyyy = today.getFullYear();
 let mm = String(today.getMonth() + 1).padStart(2, '0');
 let dd = String(today.getDate()).padStart(2, '0');
 today = `${yyyy}-${mm}-${dd}`
-const url = ``
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${today}`
 fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
